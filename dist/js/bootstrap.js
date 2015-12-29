d2cb48af61912fda614ce6c0d8549ab07cafa148
Oliver Georgi <slackero+github@gmail.com>
2015-12-29 17:24:20 +0100

    Slight updates and corrections

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -1050,7 +1050,9 @@ if (typeof jQuery === 'undefined') {
     $(document)
       .off('focusin.bs.modal') // guard against infinite focus loop
       .on('focusin.bs.modal', $.proxy(function (e) {
-        if (this.$element[0] !== e.target && !this.$element.has(e.target).length) {
+        if (document !== event.target &&
+            this.$element[0] !== e.target &&
+            !this.$element.has(e.target).length) {
           this.$element.trigger('focus')
         }
       }, this))
